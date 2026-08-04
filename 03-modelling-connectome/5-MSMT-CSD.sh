92380f23dc363ce69b276feb2226572bf1063cf9
Tanni Mobasherry <tani.nadim@yahoo.com>
2026-08-04 12:55:47 +0800

    Update 05-MSMT-CSD.sh

--- 03-modelling-connectome/5-MSMT-CSD.sh
+++ 03-modelling-connectome/5-MSMT-CSD.sh
@@ -6,7 +6,11 @@
 # Purpose:
 # Estimate the white-matter fibre orientation distribution (WM FOD)
 # together with the grey-matter and CSF tissue compartments using
-# multi-shell multi-tissue constrained spherical deconvolution.
+# multi-shell multi-tissue constrained spherical deconvolution
+# (MSMT-CSD).
+#
+# Group-average WM, GM and CSF response functions are used for all
+# subjects and time points to improve consistency across the study.
 #
 # Subject:
 #   YTH001_BL
@@ -18,12 +22,12 @@
 #   1-diffusion-preprocessing/09_bias_field_correction.sh
 #   "/Volumes/Toshiba-Ext/raw-data/YTH001/BL/dmri/preprocessing/bias-field-correction/"
 #
-#   wm.txt
-#   gm.txt
-#   csf.txt
-#   Tissue response functions generated in:
-#   3-modelling-connectome/03_response_function.sh
-#   "/Volumes/Toshiba-Ext/raw-data/YTH001/BL/dmri/modelling-connectome/response-function/"
+#   group_wm.txt
+#   group_gm.txt
+#   group_csf.txt
+#   Group-average tissue response functions generated in:
+#   3-modelling-connectome/04_group_response_function.sh
+#   "/Volumes/Toshiba-Ext/raw-data/group-response-function/"
 #
 #   mask.mif
 #   Diffusion brain mask generated from the bias-field-corrected
@@ -34,20 +38,28 @@
 # Outputs:
 #
 #   wmfod.mif
+#   White-matter fibre orientation distribution (FOD)
+#
 #   gm.mif
+#   Grey-matter tissue compartment
+#
 #   csf.mif
+#   Cerebrospinal fluid tissue compartment
+#
 #   "/Volumes/Toshiba-Ext/raw-data/YTH001/BL/dmri/modelling-connectome/MSMT-CSD/"
 # ================================================================
 
+
 # ------------------------------------------------------------
 # Step 1: Estimate WM FOD, GM and CSF tissue compartments
-#         using MSMT-CSD
+#         using MSMT-CSD with the group-average response
+#         functions
 # ------------------------------------------------------------
 
 dwi2fod msmt_csd \
     dwi_eddy_BA.mif \
-    wm.txt wmfod.mif \
-    gm.txt gm.mif \
-    csf.txt csf.mif \
+    group_wm.txt wmfod.mif \
+    group_gm.txt gm.mif \
+    group_csf.txt csf.mif \
     -mask mask.mif \
     -force
